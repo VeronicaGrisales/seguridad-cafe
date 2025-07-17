123af748cdc8138436f986baa2d5c70b9cf88b0c
VeronicaGrisales <verogrisales4@gmail.com>
2025-07-17 13:50:42 -0500

    spring boot

---
+++ cafeNazareth/src/test/java/com/example/cafeNazareth/CafeNazarethApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cafeNazareth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeNazarethApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
